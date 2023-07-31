58fb11ea3d7894f315276c8f7fd8791c026c4b88
mahdiyar ahmadizade <82575763+mahdiyar-az@users.noreply.github.com>
2023-08-01 00:29:13 +0330

    finish first idea for todo list

--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,28 @@
+let ToDO= []
+let sortedToDO = []
 function addNewTodo(){
     
-    let inputJob = document.querySelector('input.usr');
-    let date = document.querySelector('input.date');
+    let inputJob = document.querySelector('input.usr').value;
+    let dates = document.querySelector('input.date').value;
     let todos = document.querySelector('div.todos');
-    
-    todos.innerHTML = todos.innerHTML + `<div class="todo"><p>${inputJob.innerHTML}</p><p>${date.innerHTML}</p></div>`;
-    document.querySelector('div.todos').innerHTML = todos.innerHTML;
+    ToDO.push({
+        job:inputJob,
+        date:dates
+    });
+    sortedToDO = []
+    sortedToDO = ToDO.sort(
+        (p1, p2) => (p1.date > p2.date) ? 1 : (p1.date < p2.date) ? -1 : 0);
+    console.log(sortedToDO);
+    useTodo();
+   // todos.innerHTML = todos.innerHTML + `<div class="todo"><p>${inputJob}</p><p>${dates}</p></div>`;
+   // document.querySelector('div.todos').innerHTML = todos.innerHTML;
 }
+
+function useTodo(){
+    let b = '<p>To DO:</p>';
+    
+    for (const a of sortedToDO ) {
+        b = b+ `<div class="todo"><p>${a.job}</p><p>${a.date}</p></div>`;
+    }
+    document.querySelector('div.todos').innerHTML = b;
+} 
