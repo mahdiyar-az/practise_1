943c1041e38bc2742f76790482e5f60084266600
mahdiyar ahmadizade <82575763+mahdiyar-az@users.noreply.github.com>
2023-07-31 23:11:49 +0330

    new changes

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,9 @@
 function addNewTodo(){
-    let a = document.getElementById("formsInput").enctype();
     
-    alert(a);
+    let inputJob = document.querySelector('input.usr');
+    let date = document.querySelector('input.date');
+    let todos = document.querySelector('div.todos');
+    
+    todos.innerHTML = todos.innerHTML + `<div class="todo"><p>${inputJob.innerHTML}</p><p>${date.innerHTML}</p></div>`;
+    document.querySelector('div.todos').innerHTML = todos.innerHTML;
 }
