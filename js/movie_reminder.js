9b3274afc9d037de99564fc7509ab42fff7c41ec
mahdiyar ahmadizade <82575763+mahdiyar-az@users.noreply.github.com>
2023-08-07 22:46:58 +0330

    changes in add movie

--- js/movie_reminder.js
+++ js/movie_reminder.js
@@ -5,20 +5,36 @@ class movie{
         this.link = link;
         this.is_film = is_film;
     }
+
+    getName() {
+        return this.name;
+    }
+    getImdb(){
+        return this.imdb
+    }
+    getLink() {
+        return this.link;
+    }
+  
 }
 newMovie = []
 function addMovie (){
     console.log("test");
     a= document.querySelectorAll("input");
-    const name = console.log(a[0]);
-    const imdb = console.log(a[1]);
-    const link = console.log(a[2]);
+    const name = a[0];
+    const imdb = a[1];
+    const link = a[2];
     console.log(name.value);
-    b = new movie(name[value]);
+    b = new movie(name.value,imdb.value,link.value);
     newMovie.push(b);
+    movies();
 }
 
-function test(){
+function movies(){
     
+    let textfull = "";
+    for (let c of newMovie){
+        textfull = textfull + `<li>${c.getName()} + ${c.getLink()} + ${c.getImdb()}</li>`;
+    }
+    document.querySelector("ul.movies").innerHTML = textfull;
 }
-
