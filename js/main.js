7dcb95e3b1ff5c8a30cd5f1ec040bef976c339bd
mahdiyar ahmadizade <82575763+mahdiyar-az@users.noreply.github.com>
2023-08-10 00:54:26 +0330

    add nav bar to main

--- js/main.js
+++ js/main.js
@@ -2,8 +2,24 @@ icon = document.querySelector("i.navOpen");
 
 icon.addEventListener("click", myFunction);
 function myFunction(){
-    document.querySelector("nav").classList = "open";
-    const Nnavbar = document.querySelector("body div").classList;
-   
-    console.log(Nnavbar);
+    nav = document.querySelector("nav").classList;
+    
+    if (nav[0] === 'close'){
+        document.querySelector("nav").classList.replace("close","open");
+        document.querySelector("body div").classList.replace("close","open");
+        
+        for (a of document.querySelectorAll("nav a")){
+            a.classList.replace("inVisiable","visiable");
+        }
+        
+    }
+    else{
+        document.querySelector("nav").classList.replace("open","close");
+        document.querySelector("body div").classList.replace("open","close");
+        for (a of document.querySelectorAll("nav a")){
+            a.classList.replace("visiable","inVisiable");
+        }
+        document.querySelector("nav a").classList.replace("inVisiable","visiable");
+    }
+
 }
